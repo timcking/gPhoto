b89c3b945f3d15447a686a368a525408b9560de3
Tim King <timcking@gmail.com>
2013-03-06 20:47:02 -0800

    Added Login UI

--- gData.py
+++ gData.py
@@ -7,17 +7,11 @@ class gData():
     gd_client = gdata.photos.service.PhotosService()
     
     def __init__(self, passwd):
-        # Format for dictionary: {index: ('album_id')}
-        self.dictAlbum = {}
-        # Format for photo dict: {index: (photo_id, url, caption, addl_info)}
-        self.dictPhoto = {}
-        
+        # ToDo: Handle login failure
         self.gd_client.email = 'timcking@gmail.com'
         self.gd_client.password = passwd
         self.gd_client.source = 'gPhoto'
         self.gd_client.ProgrammaticLogin()
-
-        # return self.gd_client
 
     def load_albums(self):
         return self.gd_client.GetUserFeed(user='default')
